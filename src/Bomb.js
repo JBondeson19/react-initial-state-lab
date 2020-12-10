0e7e6ca38567d4a25b6901af8114e244e5b74614
JBondeson19 <jpbond58@gmail.com>
2020-12-10 16:35:13 -0500

    "initial state lab"

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,26 @@
 // your Bomb code here!
+import React from "react";
+class Bomb extends React.Component {
+    constructor(props) {
+        super()
+
+        this.state = {
+            secondsLeft: props.initialCount 
+        }
+
+
+    }
+
+    render() {
+        return(
+            <div>
+                {(this.state.secondsLeft === 0)
+                ? <h2>Boom!</h2>
+                : <h2>{this.state.secondsLeft} seconds left before I go boom!</h2>}
+            </div>
+        )
+    }
+}
+
+
+export default Bomb;
